0993afacf7716bf5574b160f27d9b05d5e992ef6
Francis Tseng <f+accounts@frnsys.com>
2018-04-03 19:25:16 -0400

    using a simpler LSH implementation

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 import boto3
-import faiss
 import numpy as np
 from PIL import Image
 from lib import models, hash
+from lshash.lshash import LSHash
 from flask import Flask, request, render_template
 
 s3 = boto3.client('s3')
 bucket_name = 'vizlab-images'
 
+MAX_RESULTS = 10
 HASH_SIZE = 16 # must be power of 2
-D = HASH_SIZE**2
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
@@ -21,28 +21,25 @@ def allowed_file(filename):
 
 
 def build_index():
+    lsh = LSHash(16, HASH_SIZE**2)
     q = session.query(models.Image.hash, models.Image.id)
-    hashes, ids = zip(*q.all())
-    hashes = [hash.normalize(hash.hex_to_hash(h).hash).flatten() for h in hashes]
-    hashes = np.stack(hashes)
-    hashes = hashes.astype('float32')
-    ids = np.array(ids)
+    for h, id in q.all():
+        h = hash.hex_to_hash(h).hash.flatten()
+        bytearr = h.view(np.uint8)
+        lsh.index(bytearr, extra_data=id)
+    return lsh
 
-    # <https://github.com/facebookresearch/faiss/wiki/Faiss-indexes>
-    # idx_ = faiss.IndexLSH(d, D*2) # this gets better more bits
-    # idx_ = faiss.IndexFlatL2(d) # best, but brute-force, and stores full vectors
-    # idx_ = faiss.IndexHNSWFlat(D, 16)
-    # see <https://github.com/facebookresearch/faiss/wiki/Getting-started-tutorial>
-    idx_ = faiss.IndexFlatIP(D) # might be best option?
-    idx = faiss.IndexIDMap(idx_) # so we can specify our own indices
-    idx.add_with_ids(hashes, ids)
 
-    # need to hold onto idx_ reference or will get a segfault
-    return idx, idx_
+def search(img):
+    h = hash.dhash(img, hash_size=HASH_SIZE)
+    query = h.hash.flatten().view(np.uint8)
+    results = lsh.query(query, num_results=MAX_RESULTS, distance_func='hamming')
+    ids_dists = [(r[0][1], r[1]) for r in results]
+    return ids_dists
 
 
 app = Flask(__name__)
-idx, idx_ = build_index()
+lsh = build_index()
 print('Done building index')
 
 
@@ -52,16 +49,15 @@ def main():
         f = request.files['image']
         if f and f.filename and allowed_file(f.filename):
             img = Image.open(f.stream)
-            h = hash.dhash(img, hash_size=HASH_SIZE)
-            query = hash.normalize(h.hash.flatten()).astype('float32')
-            dists, ids = idx.search(np.array([query]), 10)
-            dists, ids = dists[0].tolist(), ids[0].tolist()
+            results = search(img)
+            ids_to_dists = {id: dist for id, dist in results}
+            ids = list(ids_to_dists.keys())
             images = session.query(models.Image).filter(models.Image.id.in_(ids)).all()
             images = [
-                (img, dist, s3.generate_presigned_url(
+                (img, ids_to_dists[img.id], s3.generate_presigned_url(
                     'get_object',
                     Params={'Bucket': bucket_name, 'Key': img.key}, ExpiresIn=1800))
-                for img, dist in zip(images, dists)]
+                for img in images]
             return render_template('results.html', images=images)
     return render_template('index.html')
 
